bf970994236b77150b8aafc8d037b2adcab86627
arnonr <arnonr@kmutnb.ac.th>
2024-08-17 04:55:20 +0700

    update ignore log

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         new winston.transports.File({
-            filename: "error.log",
+            filename: "logs/error.log",
             level: "error",
         }),
-        new winston.transports.File({ filename: "combined.log" }),
+        new winston.transports.File({ filename: "logs/combined.log" }),
     ],
 });
 
